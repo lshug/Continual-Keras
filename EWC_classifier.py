Fixed gradient calculation, EWC loss calculation. Added Hindi char and MNIST task loading. Fixed typos.

EWC_classifier.py
@@ -10,7 +10,7 @@ def categorical_nll(y, logs):
     return -1*K.mean(tf.boolean_mask(logs,y))
 
 class EWCClassifier(ContinualClassifier):
-    def __init__(self, shape, optimizer='adam', lr=0.00001, epochs=10, loss='categorical_crossentropy', metrics=['accuracy'], singleheaded_classes=None, model={'layers':3, 'units':400,'dropout':0,'activation':'relu'}, ewc_lambda=500, fisher_n=0, empirical=False, gamma=0):
+    def __init__(self, shape, optimizer='adam', lr=0.0005, epochs=150, metrics=['accuracy'], singleheaded_classes=None, model={'layers':3, 'units':200,'dropout':0,'activation':'relu'}, ewc_lambda=500, fisher_n=0, empirical=False, gamma=0):
         self.ewc_lambda = ewc_lambda
         self.modes = []
         self.precisions = []
@@ -18,7 +18,7 @@ class EWCClassifier(ContinualClassifier):
         self.fisher_n=fisher_n
         self.empirical=empirical
         self.gamma=gamma
-        super().__init__(shape,optimizer,lr,epochs,loss,metrics,singleheaded_classes,model)
+        super().__init__(shape,optimizer,lr,epochs,categorical_nll,metrics,singleheaded_classes,model)
     
     def save_model(self, filename):
         pass
@@ -74,7 +74,7 @@ class EWCClassifier(ContinualClassifier):
                 label = wrapped_model.predict(np.array([X[i]]))[0]
             else:
                 label = Y[i]
-            gradients = K.get_session().run(K.gradients(wrapped_model.output,wrapped_model.trainable_weights), feed_dict={wrapped_model.input:np.array([X[i]])})
+            gradients = K.get_session().run(K.gradients(categorical_nll(label,wrapped_model.output),wrapped_model.trainable_weights), feed_dict={wrapped_model.input:np.array([X[i]])})
             for i in range(0,len_weights):
                 fisher_estimates[i]+=gradients[i]**2
         for i in range(0,len_weights):
@@ -111,6 +111,14 @@ class EWCClassifier(ContinualClassifier):
                 return self.ewc_lambda*0.5*K.sum((gamma*prec) * (weights-mean)**2)
             return ewc_reg
         def ewc_reg(weights):
-            return self.EWC_lambda*0.5*K.sum((prec) * (weights-mean)**2)
+            loss_total = None
+            for i in range(task_count):
+                if loss_total is None:
+                    loss_total=self.EWC_lambda*0.5*K.sum((prec[i][weight_no]) * (weights-self.means[i][weight_no])**2)
+                else:
+                    loss_total+=self.EWC_lambda*0.5*K.sum((prec[i][weight_no]) * (weights-self.means[i][weight_no])**2)
+            return loss_total
         return ewc_reg
+        
+        
 

test.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
+from keras.utils import to_categorical
+import cv2
+import tensorflow as tf
 from evaluate import ContinualClassifierEvaluator, divide_dataset_into_tasks
 from EWC_classifier import EWCClassifier
 from keras.datasets import mnist
+import os
 
 
 #df = pd.read_csv('merged1000')
@@ -17,10 +21,36 @@ from keras.datasets import mnist
 #Y = np.array([0]*10 + [1]*10 +[2]*10 + [3]*10 + [4]*10 + [5]*10 + [6]*10 + [7]*10 + [8]*10 + [9]*10)
 #tasks, labels = divide_dataset_into_tasks(X,Y,5)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-X = x_train.reshape(60000,784)/255.0
-tasks, labels = divide_dataset_into_tasks(X,y_train,5)
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#X = x_train.reshape(60000,784)/255.0
+#tasks, labels = divide_dataset_into_tasks(X,y_train,5)
 
+def get_hindi_tasks(path = 'vowels'): # vowels or numerals
+    if path == "vowels":
+        labels = ['a', 'aa', 'i', 'ee',  'u', 'oo', 'ae', 'ai', 'o', 'au', 'an', 'ah']
+    num_labels = []
+    for num, val in enumerate(labels):
+        num_labels.append(num)
+    folders = [i+1 for i in num_labels] # adding ones, since folders are arranged from 1-12
+    X = []
+    y = []
+    for f in folders:
+        abs_path = os.path.join(path, str(f))
+        for img in os.listdir(abs_path):
+            X.append(cv2.imread(os.path.join(abs_path, img), cv2.IMREAD_GRAYSCALE))
+            y.append(f - 1) # converting fodler value to original label val
+    X = np.array(X)
+    y = np.array(y)
+    print(X.shape, y.shape)
+    X = np.split(X, 4)
+    y = to_categorical(y)
+    y = np.split(y, 4)
+    # normalizing the input data modify if necessary
+    X = tf.keras.utils.normalize(X, axis = 1)
+    return X, y
+
+X, Y = get_hindi_tasks()
+X = X.reshape(4,663,784)/255.0
 ewc = EWCClassifier((X.shape[1],),fisher_n=100)
 evaluator = ContinualClassifierEvaluator(ewc, tasks, labels)
 evaluator.train()