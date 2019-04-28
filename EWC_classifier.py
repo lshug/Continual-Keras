Optimized gradient calculation for fisher approximation. Fixed multi-headed. Added evaluation method.

EWC_classifier.py
@@ -5,6 +5,7 @@ from keras.layers import Lambda
 import tensorflow as tf
 import keras.backend as K
 from sklearn.utils import shuffle
+from tqdm import tqdm
 
 def categorical_nll(y, logs):
     return -1*K.mean(tf.boolean_mask(logs,y))
@@ -27,7 +28,7 @@ class EWCClassifier(ContinualClassifier):
     def load_model(self, filename):
         pass
     
-    def task_fit_method(self, X, Y, validation_data=None, verbose=2):
+    def task_fit_method(self, X, Y, model, validation_data=None, verbose=2):
         i = 0
         j = 0
         while True:
@@ -41,11 +42,6 @@ class EWCClassifier(ContinualClassifier):
                 i+=1
             except:
                 break
-        
-        if self.singleheaded:
-            model = self.model
-        else:
-            model = self.models[-1]
         model.fit(X,Y,epochs = self.epochs, verbose=verbose, validation_data = validation_data, shuffle=True)
         self.estimate_fisher(X,Y)
     
@@ -68,15 +64,28 @@ class EWCClassifier(ContinualClassifier):
         fisher_n = self.fisher_n
         if self.fisher_n is 0 or self.fisher_n>X.shape[0]:
             fisher_n = X.shape[0]
-        for i in range(0,fisher_n):
+        
+        X=X[0:fisher_n]
+        if self.empirical is False:            
+            label=wrapped_model.predict(X)
+        else:
+            label=Y[0:fisher_n]
+            
+        #for each x,y pair, count the gradient with respect to the loss.
+        gradients = []
+        sess=K.get_session()
+        y_placeholder = tf.placeholder(tf.float32, shape=Y[0].shape)
+        grads_tesnor = K.gradients(categorical_nll(y_placeholder,wrapped_model.output),wrapped_model.trainable_weights)
+        for i in tqdm(range(fisher_n)):
             print('Fish no. {} out of {}'.format(i,fisher_n))
-            if self.empirical is False:
-                label = wrapped_model.predict(np.array([X[i]]))[0]
-            else:
-                label = Y[i]
-            gradients = K.get_session().run(K.gradients(categorical_nll(label,wrapped_model.output),wrapped_model.trainable_weights), feed_dict={wrapped_model.input:np.array([X[i]])})
-            for i in range(0,len_weights):
-                fisher_estimates[i]+=gradients[i]**2
+            gradients.append(sess.run(grads_tesnor, feed_dict={y_placeholder:Y[i],wrapped_model.input:np.array([X[i]])}))
+        
+        
+        
+        for i in tqdm(range(fisher_n)):
+            for j in range(len_weights):
+                fisher_estimates[j]+=gradients[i][j]**2
+        
         for i in range(0,len_weights):
             fisher_estimates[i]=fisher_estimates[i]/fisher_n
         

continual_classifier.py
@@ -52,7 +52,7 @@ class ContinualClassifier(ABC):
             self.model=model
             
 #    @abstractmethod
-    def task_fit_method(self, X, Y, validation_data=None, verbose=0):
+    def task_fit_method(self, X, Y, model, validation_data=None, verbose=0):
         print('No task fit method')
         
         
@@ -60,21 +60,41 @@ class ContinualClassifier(ABC):
     def save_model(self, filename):
         pass        
         
-    def task_model(self,task):
+    def task_model(self,task=-1):
         if self.singleheaded:
             return self.model
         else:
             return self.models[task]
     
-    def task_fit(self, X, Y, validation_data=None, verbose=0):
+    def task_fit(self, X, Y, task=None, validation_data=None, verbose=0):
         if not self.singleheaded:
-            x = Dense(Y.shape[1],activation='softmax',name='output_task%d'%(len(self.models)+1))(self.model.output)
-            task_Model = Model(self.model.input,x)
-            task_Model.compile(loss=self.loss,optimizer=self.optimizer,metrics=self.metrics)
-            self.models.append(task_Model)
-        self.task_fit_method(X,Y,validation_data,verbose)
+            if task is None:
+                raise Exception('Task number should be provided in task_fit if the model is not singleheaded')
+            Y=Y[:,np.sum(Y,0)!=0]
+            try:
+                model = self.task_model(task)
+            except:
+                x = Dense(Y.shape[1],activation='softmax',name='output_task%d'%(len(self.models)+1))(self.model.output)
+                task_Model = Model(self.model.input,x)
+                task_Model.compile(loss=self.loss,optimizer=self.optimizer,metrics=self.metrics)
+                self.models.append(task_Model)
+                model = task_Model
+        self.task_fit_method(X,Y,model,validation_data,verbose)
     
-
+    def evaluate(self,X,Y,task=None):
+        if not self.singleheaded:
+            if task is None:
+                raise Exception('Task number should be provided in evaluate if the model is not singleheaded')
+            Y=Y[:,np.sum(Y,0)!=0]
+            try:
+                model = self.task_model(task)
+            except:
+                x = Dense(Y.shape[1],activation='softmax',name='output_task%d'%(len(self.models)+1))(self.model.output)
+                task_Model = Model(self.model.input,x)
+                task_Model.compile(loss=self.loss,optimizer=self.optimizer,metrics=self.metrics)
+                self.models.append(task_Model)
+                model = task_Model
+        return self.task_model(task).evaluate(X,Y)
         
 #    @abstractmethod
     def load_model(self, filename):

evaluate.py
@@ -48,9 +48,9 @@ class ContinualClassifierEvaluator():
     def train(self,verbose=2):
         for i in range(len(self.tasks)):
             print('Training on task %d'%i)
-            self.classifier.task_fit(self.tasks[i],self.labels[i],verbose=verbose)
+            self.classifier.task_fit(self.tasks[i],self.labels[i],i,verbose=verbose)
             for j in range(len(self.tasks)):
-                self.accuracies[i,j] = self.classifier.task_model(i).evaluate(self.tasks[j],self.labels[j])[1]
+                self.accuracies[i,j] = self.classifier.evaluate(self.tasks[j],self.labels[j],j)[1]
     
     def evaluate(self):
         ACC = np.sum(self.accuracies[-1])/len(self.tasks)
@@ -68,7 +68,7 @@ class ContinualClassifierEvaluator():
         
         FWT = 0
         for i in range(1,len(self.tasks)):
-            FWT+=self.accuracies[i,i] - self.classifier.task_model(i).evaluate(self.tasks[i],self.labels[i])[1]
+            FWT+=self.accuracies[i,i] - self.classifier.evaluate(self.tasks[i],self.labels[i],i)[1]
         FWT = FWT/(len(self.tasks)-1)    
         self.classifier.model.set_weights(trained_weights)
         