d4166978c998ce1c4f520b069f9200576698b945
DadianisBidza <kutxisdaxra@outlook.com>
2019-04-29 11:00:54 +0400

    Added preprocessing to permute_mnist

--- permute_mnist.py
+++ permute_mnist.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 import numpy as np
+from tqdm import tqdm
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-
-
-def permute(task_no):
+x_train=x_train.reshape(60000,784)/255.0
+y_train=to_categorical(y_train)
+def get_permute_mnist_tasks(task_no):
     tasks = []
     tasks.append(x_train)
     labels = []
     labels.append(y_train)
-    for i in range(task_no-1):
+    for i in tqdm(range(task_no-1)):
         tasks.append(np.apply_along_axis(np.random.permutation,1,x_train))
         labels.append(y_train)
     return tasks, labels
--- test.py
+++ test.py
@@ -11,15 +11,22 @@ from evaluate import ContinualClassifierEvaluator, divide_dataset_into_tasks
 from EWC_classifier import EWCClassifier
 from keras.datasets import mnist
 import os
+from permute_mnist import get_permute_mnist_tasks
+
+task ='mnist'
 
 
+if task is 'mnist':
+    #divided mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    X = x_train.reshape(60000,784)/255.0
+    tasks, labels = divide_dataset_into_tasks(X,y_train,5)
+    
+if task is 'permnist':
+    tasks, labels = get_permute_mnist_tasks(20)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-X = x_train.reshape(60000,784)/255.0
-tasks, labels = divide_dataset_into_tasks(X,y_train,5)
 
-
-ewc = EWCClassifier((X.shape[1],),fisher_n=3000,epochs=10,ewc_lambda=500,lr=0.00005,empirical=True)
+ewc = EWCClassifier((tasks[0].shape[1],),fisher_n=3000,epochs=10,ewc_lambda=500,lr=0.00005,empirical=True)
 evaluator = ContinualClassifierEvaluator(ewc, tasks, labels)
 evaluator.train()
 evaluator.evaluate()
